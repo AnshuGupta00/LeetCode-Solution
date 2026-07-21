dfca6f097cb8a34df54d85e0412a9d7079fbfbfe
Anshu Gupta <144334045+AnshuGupta00@users.noreply.github.com>
2026-07-21 16:02:08 +0530

    Add solution for 3499. Maximize Active Section with Trade I

--- 3499-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.java
@@ -1,23 +1,23 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
-        int n =s.length();
-        int ones =0, maxsum=0, prevrun=Integer.MIN_VALUE;
-        int i=0;
-        while(i<n){
-            if(s.charAt(i)=='1'){
+        int n = s.length();
+        int ones = 0, maxsum = 0, prevrun = Integer.MIN_VALUE;
+        int i = 0;
+        while (i < n) {
+            if (s.charAt(i) == '1') {
                 ones++;
                 i++;
-            }else{
-                int curr=0;
-                while(i<n && s.charAt(i)=='0'){
+            } else {
+                int curr = 0;
+                while (i < n && s.charAt(i) == '0') {
                     curr++;
                     i++;
                 }
-                    maxsum=Math.max(maxsum,prevrun+curr);
-                    prevrun=curr;
+                maxsum = Math.max(maxsum, prevrun + curr);
+                prevrun = curr;
             }
 
         }
-        return ones+maxsum;
+        return ones + maxsum;
     }
 }
