874204db7ebeedf46fffe52e17164d45d35f2c58
Anshu Gupta <144334045+AnshuGupta00@users.noreply.github.com>
2026-08-03 23:24:53 +0530

    Add solution for 1406. Stone Game III

--- 1406-stone-game-iii/stone-game-iii.java
+++ 1406-stone-game-iii/stone-game-iii.java
@@ -1,13 +1,13 @@
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
-        int n =stoneValue.lenght;
+        int n =stoneValue.length;
         int[] dp= new int[n+1];
         
         for(int i=n-1; i<=0; i--){
             dp[i]=Integer.MIN_VALUE;
             int sum=0;
         
-        for(int k=0; k<3; && i+k<n; k++){
+        for(int k=0; k<3 && i+k<n; k++){
             sum +=stoneValue[i+k];
             dp[i]=Math.max(dp[i],sum -dp[i +k+1]);
         }
@@ -15,6 +15,6 @@ class Solution {
 
     if(dp[0]> 0) return "Alice";
     if(dp[0]< 0) return "Bob";
-    return Tie;
+    return "Tie";
     }
 }
