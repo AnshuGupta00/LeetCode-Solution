0f515786bfd41e15a26aadfefa958410d85cd407
Anshu Gupta <144334045+AnshuGupta00@users.noreply.github.com>
2026-08-06 15:28:35 +0530

    Codeship: Added solution for Remove Methods From Project

---
+++ problems/remove-methods-from-project/solution.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        //Step 1: Creating Adjency List,
+        List<List<Integer>> adjL= new ArrayList<>();
+        for(int method=0; method<n; method++) adjL.add(new ArrayList<>());
+        for(int[] edge: invocations){
+            adjL.get(edge[0]).add(edge[1]);
+        }
+        //Step 2: Mark Method by reachable by bug Method
+        boolean[] isSuspicious=new boolean[n];
+        Deque <Integer> stack= new ArrayDeque<>();
+        stack.push(k);
+        isSuspicious[k]=true;
+        while(!stack.isEmpty()){
+    
+            int currenMethod=stack.pop();
+            for(int calledMethod:adjL.get(currenMethod)){
+                if(!isSuspicious[calledMethod]){
+                    isSuspicious[calledMethod]=true;
+                    stack.push(calledMethod);
+                }
+            }
+        }
+    //Step 3:
+    for(int[]edge:  invocations){
+        int caller=edge[0], called=edge[1];
+        if(!isSuspicious[caller] && isSuspicious[called]){
+            List all=new ArrayList<>();
+            for(int m=0; m<n; m++){
+                all.add(m);
+            }
+            return all;
+        }
+    }
+    //step 4:
+    List<Integer> result =new ArrayList<>();
+    for(int m =0; m<n; m++){
+        if(!isSuspicious[m]){
+             result.add(m);
+    }
+    }
+    return result;
+   
+    }
+   }
