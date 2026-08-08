32c59d308839058c17a84898003fb379a1bb3347
Anshu Gupta <144334045+AnshuGupta00@users.noreply.github.com>
2026-08-09 00:51:17 +0530

    Add solution for 3302. Find the Lexicographically Smallest Valid Sequence

--- 3302-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
+++ 3302-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] validSequence(String word1, String word2) {
          int n =word1.length();
-         int m =word1.length();
+         int m =word2.length();
 
 
         int[] last= new int [m];
         Arrays.fill(last,-1);
         int j=m-1;
         for (int i=n-1; i >=0; --i){
-             if(j<=0 && word1.charAt(i) == word2.charAt(j)){
+             if(j>=0 && word1.charAt(i) == word2.charAt(j)){
                 last[j]=i;
                 j-=1;
              }
